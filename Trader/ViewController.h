1cd8b3774dc519855a9fbdb59fefc6df899e4bdd
berk <berk@berks-iMac.local>
2018-02-03 07:52:33 -0500

    Initial Commit

---
+++ Trader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trader
+//
+//  Created by berk on 2/3/18.
+//  Copyright © 2018 berk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
